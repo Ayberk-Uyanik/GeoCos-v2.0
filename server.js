7d0a372a0877fc343dfa71aa72e33d1d5c500258
Ayberk Uyanik <67552104+Ayberk-Uyanik@users.noreply.github.com>
2024-09-16 17:22:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require('express');
 const app = express();
 
 // Static Files //
-app.use(express.static('public'));
-app.use ('/css', express.static(__dirname + 'public/css'));
-app.use ('/img', express.static(__dirname + 'public/img'));
-app.use ('/js', express.static(__dirname + 'public/js'));
+app.use(express.static('/public'));
+app.use ('/css', express.static(__dirname + '/public/css'));
+app.use ('/img', express.static(__dirname + '/public/img'));
+app.use ('/js', express.static(__dirname + '/public/js'));
 
 // Rendering Pages //
 app.get('/public', (req, res) => {
